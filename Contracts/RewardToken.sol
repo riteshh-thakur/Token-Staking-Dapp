91d7d59b6556472c0a89b9f11a90f59e72cb42b6
Ritesh Thakur <134767629+riteshh-thakur@users.noreply.github.com>
2025-02-28 18:06:04 +0530

    Update RewardToken.sol

--- Contracts/RewardToken.sol
+++ Contracts/RewardToken.sol
@@ -1,1 +1,12 @@
+// contracts/GLDToken.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract RewardToken is ERC20 {
+    constructor(uint256 initialSupply) ERC20("RewardToken", "RWT") {
+        _mint(msg.sender, initialSupply*10**18);
+    }
+
+}
